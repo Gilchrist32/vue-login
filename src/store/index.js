5e32514b7e812951ae6b52d1837ea2f8a02bcb66
Gilchrist32 <32stephencurry@gmail.com>
2020-07-31 00:01:57 +0800

    Added Vuex Store

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    mode: true
   },
   mutations: {
+    DARK_MODE(state) {
+      state.mode = !state.mode
+    }
   },
   actions: {
+    darkMode({ commit }) {
+      commit('DARK_MODE')
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
