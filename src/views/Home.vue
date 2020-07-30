3ac416239c56374836d3920753c02cfc6fd86449
Gilchrist32 <32stephencurry@gmail.com>
2020-07-30 22:05:16 +0800

    Changes Home Sign Up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card class="elevation-12">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1 class="text-center display-2 teal--text text--accent-3">Sign in</h1>
+                        <div class="text-center" mt-4>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-instagram</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-github</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mlt-4">Ensure your email for registration</h4>
+                        <v-form>
+                          <v-text-field
+                            label="Email Address"
+                            name="Email"
+                            prepend-icon="email"
+                            type="text"
+                            color="teal accent-3"
+                           />
+                           <v-text-field
+                            id="password"
+                            label="Password"
+                            name="Password"
+                            prepend-icon="lock"
+                            type="password"
+                            color="teal accent-3"
+                            />
+                        </v-form>
+                        <h3 class="text-center mt-3">Forgot your password</h3>
+                      </v-card-text>
+                      <div class="text-center mt-3">
+                        <v-btn rounded color="teal accent-3" dark>SIGN IN</v-btn>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">!Hello, Friends !</h1>
+                        <h5 class="text-center">Enter your personnel details and start journey with us</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined="" dark @click="step++">SIGN UP</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2">
+
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  
+  data: () => ({
+    step: 1
+  }),
+  props: {
+    source: String
   }
 }
 </script>
