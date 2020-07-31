f6ee86f4683ee6df3ee48781f0d2b205eefd7bc3
Gilchrist32 <32stephencurry@gmail.com>
2020-07-31 16:16:29 +0800

    Added App bar and Computed Vuex

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,39 @@
 <template>
   <v-app>
+     <v-app-bar
+       absolute 
+      :color="mode ? 'success white--text' : ''"
+      elevate-on-scroll
+      :dark="mode ? false : true"
+      >
+      <v-toolbar-title>
+        MeetUp
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn class="white--text mr-2"
+          depressed
+          :color="mode ? 'success white--text' : ''"
+          v-on="on"
+          to="/">
+          <v-icon left class="white--text">mdi-home</v-icon>
+          Home
+          </v-btn>
+          </template>
+      </v-tooltip>
+      
+          <v-btn
+          class="white--text"
+          depressed
+          :color="mode ? 'success white--text' : ''"
+          to="/login">
+          <v-icon left>mdi-login</v-icon>
+          Login
+          </v-btn>
+      </v-app-bar>
     <v-content>
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
@@ -46,23 +80,23 @@
                         <h3 class="text-center mt-3">Forgot your password</h3>
                       </v-card-text>
                       <div class="text-center mt-3">
-                        <v-btn rounded color="teal accent-3" dark>SIGN IN</v-btn>
+                        <v-btn rounded color="success" dark>SIGN IN</v-btn>
                       </div>
                     </v-col>
-                    <v-col cols="12" md="4" class="teal accent-3">
+                    <v-col cols="12" md="4" class="success">
                       <v-card-text class="white--text mt-12">
                         <h1 class="text-center display-1">!Hello, Friends !</h1>
                         <h5 class="text-center">Enter your personnel details and start journey with us</h5>
                       </v-card-text>
                       <div class="text-center">
-                        <v-btn rounded outlined dark @click="step++">SIGN UP</v-btn>
+                        <v-btn rounded outlined dark @click="step++">CREAtE ACCOUNT</v-btn>
                       </div>
                     </v-col>
                   </v-row>
                 </v-window-item>
-                <v-window-item :value="2">
+                <v-window-item :value="2" class="mt-12">
                   <v-row class="fill-height">
-                    <v-col cols="12" md="4" class="teal accent-3">
+                    <v-col cols="12" md="4" class="success">
                       <v-card-text class="white--text mt-12">
                         <h1 class="text-center display-1">Welcome Back !</h1>
                         <h5 class="text-center">To Keep connected with us please login with your personnel info</h5>
@@ -137,6 +171,7 @@
   </v-app>
 </template>
 <script>
+import { mapState } from 'vuex' 
 export default {
   name: 'Login',
   
@@ -145,6 +180,10 @@ export default {
   }),
   props: {
     source: String
-  }
+  },
+
+  computed: {
+    ...mapState(['mode'])
+  },
 }
 </script>
